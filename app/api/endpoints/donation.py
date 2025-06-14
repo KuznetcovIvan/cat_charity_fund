7c2ffcfcc804fac00c96718c941eb00111f25c1a
Ivan Kuznetcov <kuz_master@mail.ru>
2025-06-14 12:39:51 +0300

    Add create_new_donation

--- app/api/endpoints/donation.py
+++ app/api/endpoints/donation.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from crud.donation import donation_crud
 from app.core.db import get_async_session
-from app.core.user import current_superuser
-from app.schemas.donation import DonationDB
+from app.core.user import current_superuser, current_user
+from app.schemas.donation import DonationDB, DonationDBShort, DonationCreate
+from app.models.user import User
+
 
 router = APIRouter()
 
@@ -18,3 +20,16 @@ async def get_all_donations(
     session: AsyncSession = Depends(get_async_session)
 ):
     return await donation_crud.get_multi(session)
+
+
+@router.post(
+    '/',
+    response_model=DonationDBShort,
+    response_model_exclude_none=True,
+)
+async def create_new_donation(
+    donation: DonationCreate,
+    session: AsyncSession = Depends(get_async_session),
+    user: User = Depends(current_user)
+):
+    return await donation_crud.create(donation, session, user)
